8fb93bb73269dd60702d378088cdfbf20ead6918
saket kumar <saketbittu012@gmail.com>
2021-04-24 22:16:50 +0530

    Basic page created

---
+++ src/Components/ProductList/index.js
@@ -0,0 +1,21 @@
+import './index.css';
+
+function ProductList() {
+  return (
+    <div className="Container">
+      <header className="Category">
+        <select>
+            <option>Fruits</option>
+            <option>Vagitable</option>
+        </select>
+      </header>
+      <div className="Carousel">
+          <div>xyd</div>
+          <div>xyd</div>
+          <div>xyd</div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductList;
---
+++ src/Constant/itemList.js
@@ -0,0 +1,93 @@
+export default items = [
+    {
+        image: '',
+        name: 'Cucumber',
+        category: 'vagitable',
+        price: 10
+    },
+    {
+        image: '',
+        name: 'carrot',
+        category: 'vagitable',
+        price: 11
+    },
+    {
+        image: '',
+        name: 'broccoli',
+        category: 'vagitable',
+        price: 14
+    },
+    {
+        image: '',
+        name: 'asparagus',
+        category: 'vagitable',
+        price: 17
+    },
+    {
+        image: '',
+        name: 'cauliflower',
+        category: 'vagitable',
+        price: 19
+    },
+    {
+        image: '',
+        name: 'corn',
+        category: 'vagitable',
+        price: 11
+    },
+    {
+        image: '',
+        name: 'cucumber',
+        category: 'vagitable',
+        price: 10
+    },
+    {
+        image: '',
+        name: 'eggplant',
+        category: 'vagitable',
+        price: 20
+    },
+    {
+        image: '',
+        name: 'green pepper',
+        category: 'vagitable',
+        price: 10
+    },
+    
+    {
+        image: '',
+        name: 'grapes',
+        category: 'fruits',
+        price: 50
+    },
+    {
+        image: '',
+        name: 'lime',
+        category: 'fruits',
+        price: 50
+    },
+    {
+        image: '',
+        name: 'lemon',
+        category: 'fruits',
+        price: 50
+    },
+    {
+        image: '',
+        name: 'banana',
+        category: 'fruits',
+        price: 50
+    },
+    {
+        image: '',
+        name: 'apple',
+        category: 'fruits',
+        price: 50
+    },
+    {
+        image: '',
+        name: 'watermelon',
+        category: 'fruits',
+        price: 50
+    },
+]
