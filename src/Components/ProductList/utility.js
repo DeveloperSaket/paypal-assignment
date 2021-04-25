c86d737d87740260822741d3072cf9a6f63ccc29
saket kumar <saketbittu012@gmail.com>
2021-04-25 18:13:57 +0530

    logic changed for next prev

--- src/Components/ProductList/utility.js
+++ src/Components/ProductList/utility.js
@@ -10,53 +10,81 @@ export function current(list = []) {
 export function prev(list = [], currentIndexPointer) {
     const length = list.length;
     const indexPointer = currentIndexPointer - 1;
+    if (length > 3) {
+        let lst = [];
+        switch (indexPointer) {
+            case 0:
+                lst = list.slice(indexPointer, indexPointer + 2);
+                lst.unshift(list[length - 1]);
+                return { list: lst, pointer: indexPointer };
+            case -1:
+                lst = list.slice(length - 2, length);
+                lst.push(list[0]);
+                return { list: lst, pointer: length - 1 };
+            default:
+                return { list: list.slice(indexPointer - 1, indexPointer + 2), pointer: indexPointer };
+        }
 
-    if (indexPointer === -1) {
-        return false; // no change required
+    } else {
+
     }
-    if (indexPointer === 0) {
-        return length < 2 ? { list, pointer: indexPointer } : { list: list.slice(0, 1), pointer: indexPointer };
-    } else if (length < 4) {
-        return { list, pointer: indexPointer };
-    } else if (length > indexPointer + 5) {
-        return { list: list.slice(length - 4, length - 1), pointer: indexPointer };
-    }
-    return { list: list.slice(indexPointer + 1, indexPointer + 4), pointer: indexPointer };
+
+    // return { list: list.slice(indexPointer, indexPointer + 3), pointer: indexPointer };
 }
 
 export function next(list, currentIndexPointer) {
     const length = list.length;
     const indexPointer = currentIndexPointer + 1;
+    if (length > 3) {
+        let lst = [];
+        switch (indexPointer) {
+            case length - 1:
+                lst = list.slice(length - 2, length);
+                lst.push(list[0]);
+                return { list: lst, pointer: indexPointer };
+            case length:
+                lst = list.slice(0, 2);
+                lst.unshift(list[length - 1]);
+                return { list: lst, pointer: 0 };
+            default:
+                return { list: list.slice(indexPointer - 1, indexPointer + 2), pointer: indexPointer };
+        }
 
-    if (indexPointer === length) {
-        return false; // no change required
+    } else {
+
     }
-    if (indexPointer === length - 1) {
-        return length < 2 ? { list, pointer: indexPointer } : { list: list.slice(0, 1), pointer: indexPointer };
-    } else if (length < 4) {
-        return { list, pointer: indexPointer };
-    } else if (length > indexPointer + 5) {
-        return { list: list.slice(length - 4, length - 1), pointer: indexPointer };
-    }
-    return { list: list.slice(indexPointer + 1, indexPointer + 4), pointer: indexPointer };
 }
 
-// export function prev(list = [], indexStart) {
-//     const length = list.length();
-//     if (length < 4) {
-//         return list;
-//     } else if (length > indexStart + 5) {
-//         return list.slice(length - 4, length - 1);
+// export function prev(list = [], currentIndexPointer) {
+//     const length = list.length;
+//     const indexPointer = currentIndexPointer - 1;
+
+//     if (indexPointer === -1) {
+//         return false; // no change required
 //     }
-//     return list.slice(indexStart + 1, indexStart + 4);
+//     if (indexPointer === 0) {
+//         return length < 2 ? { list, pointer: indexPointer } : { list: list.slice(0, 1), pointer: indexPointer };
+//     } else if (length < 4) {
+//         return { list, pointer: indexPointer };
+//     } else if (length > indexPointer + 5) {
+//         return { list: list.slice(length - 4, length - 1), pointer: indexPointer };
+//     }
+//     return { list: list.slice(indexPointer + 1, indexPointer + 4), pointer: indexPointer };
 // }
 
-// export function next(list, indexStart) {
-//     const length = list.length();
-//     if (length < 4) {
-//         return list;
-//     } else if (length > indexStart + 5) {
-//         return list.slice(length - 4, length - 1);
+// export function next(list, currentIndexPointer) {
+//     const length = list.length;
+//     const indexPointer = currentIndexPointer + 1;
+
+//     if (indexPointer === length) {
+//         return false; // no change required
 //     }
-//     return list.slice(indexStart + 1, indexStart + 4);
+//     if (indexPointer === length - 1) {
+//         return length < 2 ? { list, pointer: indexPointer } : { list: list.slice(0, 1), pointer: indexPointer };
+//     } else if (length < 4) {
+//         return { list, pointer: indexPointer };
+//     } else if (length > indexPointer + 5) {
+//         return { list: list.slice(length - 4, length - 1), pointer: indexPointer };
+//     }
+//     return { list: list.slice(indexPointer + 1, indexPointer + 4), pointer: indexPointer };
 // }
