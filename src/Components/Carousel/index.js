f4370ed1a1be6aab32a466bbb1a4ea175233523a
saket kumar <saketbittu012@gmail.com>
2021-04-25 23:53:29 +0530

    roughly add animation

--- src/Components/Carousel/index.js
+++ src/Components/Carousel/index.js
@@ -1,12 +1,13 @@
 import './index.css';
 import PropTypes from "prop-types";
+// import ReactCSSTransitionGroup from 'react-transition-group';
 
-function Carousel({ itemObj, filteredList, cur = 2 }) {
+function Carousel({ itemObj, filteredList }) {
     const { list, pointer } = itemObj;
 
     return (
         <div className='container'>
-            <div className='card-container'>
+            <div className={pointer % 2 === 0 ? 'card-container' : 'card-container2'}>
                 {list.map((i, index) => {
                     return (<div className={index !== 1 ? 'card' : 'card-selected'}>
                         <div className='cardImage'></div>
@@ -22,7 +23,7 @@ function Carousel({ itemObj, filteredList, cur = 2 }) {
                 }
                 )}
             </div>
-        </div>
+        </div >
     );
 }
 
